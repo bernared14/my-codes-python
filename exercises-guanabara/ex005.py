8397f875b62de712394704370bf71e9ec41a5586
bernared14 <81765951+bernared14@users.noreply.github.com>
2025-11-01 21:22:30 -0400

    Create ex005.py

---
+++ exercises-guanabara/ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor. 🇧🇷
+# Write a program that reads an integer and displays its successor and predecessor on the screen. 🇺🇸
+# Escriba un programa que lea un número entero y muestre su sucesor y predecesor en la pantalla. 🇪🇸
+# Écrivez un programme qui lit un entier et affiche son successeur et son prédécesseur à l'écran. 🇫🇷
+
+n = int(input("Enter an integer: "))
+print(f"The successor number of {n} is {n+1}, and its predecessor is {n-1}.")
